96015e2d75a8cde4197cfb3caca22ceaadc70605
Jimmy Castro <ja.castros@javeriana.edu.co>
2023-04-17 22:42:58 -0500

    Initial commit

---
+++ src/main/java/co/com/cattleya/auth/AuthenticationProyectoApplication.java
@@ -0,0 +1,13 @@
+package co.com.cattleya.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationProyectoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenticationProyectoApplication.class, args);
+    }
+
+}
